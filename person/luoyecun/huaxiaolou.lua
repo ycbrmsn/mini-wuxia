01cc02dd83dcfbc6c0cf23bb38be2728d597a2c8
JiangZhiwei <judgezerowit763@163.com>
2020-04-03 08:14:24 +0800

    加上花小楼碰撞事件~~~

---
+++ person/luoyecun/huaxiaolou.lua
@@ -0,0 +1,33 @@
+-- 花小楼
+Huaxiaolou = MyActor:new(huaxiaolouActorId)
+
+function Huaxiaolou:new ()
+  local o = {
+    objid = 4301071935,
+    initPosition = { x = 10, y = 10, z = -43 }
+  }
+  setmetatable(o, self)
+  self.__index = self
+  return o
+end
+
+-- 默认想法
+function Huaxiaolou:defaultWant ()
+  self:wantDoNothing()
+end
+
+-- 在几点想做什么
+function Huaxiaolou:wantAtHour (hour)
+
+end
+
+-- 初始化
+function Huaxiaolou:init (hour)
+  self:initActor(self.initPosition)
+end
+
+
+function Huaxiaolou:collidePlayer (playerid, isPlayerInFront)
+  local nickname = PlayerHelper:getNickname(playerid)
+  self.action:speak(nickname .. '，你撞我我也不给你好吃的。', playerid)
+end
