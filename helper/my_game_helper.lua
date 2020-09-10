bc058b8f438f046ffaba7f6789e06e1487649a80
JiangZhiwei <judgezerowit763@163.com>
2020-09-10 11:32:47 +0800

    修改结局描述~~~

--- helper/my_game_helper.lua
+++ helper/my_game_helper.lua
@@ -5,10 +5,15 @@ function MyGameHelper:setGBattleUI ()
   local player = PlayerHelper:getHostPlayer()
   if (player) then
     MusicHelper:stopBGM(player.objid)
-    local title, desc1, desc2 = MyGameHelper:getDesc(player)
-    UIHelper:setLeftDesc(desc1, '#G', player:getName(), '#n', desc2)
-    UIHelper:setLeftLittleDesc('玩家等级：', player:getLevel())
-    UIHelper:setRightLittleDesc('当前剧情：', StoryHelper:getStory().title)
+    local title, desc, reason = MyGameHelper:getDesc(player)
+    UIHelper:setLeftDesc(desc)
+    if (reason) then
+      UIHelper:setLeftLittleDesc('死亡原因：')
+      UIHelper:setRightLittleDesc(reason)
+    else
+      UIHelper:setLeftLittleDesc('成功通关')
+      UIHelper:setRightLittleDesc('等级：', player:getLevel())
+    end
     UIHelper:setLeftTitle('获得称号：')
     UIHelper:setRightTitle(title)
   end
@@ -17,22 +22,22 @@ end
 
 function MyGameHelper:getDesc (player)
   if (player.isWinGame) then
-    return '一代大侠', '经过一番努力，', '远近闻名于天下'
+    return '一代大侠', '闯江湖，走遍天下恶尽除'
   else
     local finalBeatBy = player.finalBeatBy
     if (not(finalBeatBy)) then
-      return '冒险家', '天有不测风云，', '意外死掉了'
+      return '冒险家', '孰能料，蹦蹦跳跳会死掉', '意外'
     else -- 被生物击败
       if (finalBeatBy == MyMap.ACTOR.DOG_ACTOR_ID) then
-        return '战五渣', '难以置信，', '竟然被野狗给咬死了'
+        return '战五渣', '难置信，野狗也能要人命', '野狗'
       elseif (finalBeatBy == MyMap.ACTOR.WOLF_ACTOR_ID) then
-        return '喂狼者', '真善良，', '一身皮囊喂了恶狼'
+        return '喂狼者', '真善良，舍去皮囊喂了狼', '恶狼'
       elseif (finalBeatBy == MyMap.ACTOR.OX_ACTOR_ID) then
-        return '斗牛逝', '没轻功，', '终靠狂牛上天空'
+        return '斗牛逝', '没轻功，依然能够上天空', '狂牛'
       elseif (finalBeatBy == MyMap.ACTOR.QIANGDAO_LOULUO_ACTOR_ID) then
-        return '守财奴', '为守住钱财，', '将命交给了强盗喽罗'
+        return '守财奴', '为守财，美好人间又白来', '强盗小喽罗'
       elseif (finalBeatBy == MyMap.ACTOR.QIANGDAO_XIAOTOUMU_ACTOR_ID) then
-        return '锤死者', '没想到，', '被强盗小头目两拳锤死了'
+        return '锤死者', '功夫差，竟被两拳锤趴下', '强盗小头目'
       end
     end
   end
