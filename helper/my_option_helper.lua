f5716fb1e00edfb9b8076193511fe654e0fc76be
JiangZhiwei <judgezerowit763@163.com>
2021-10-02 22:43:59 +0800

    选择选项后移除不能点击的间隔时间~

--- helper/my_option_helper.lua
+++ helper/my_option_helper.lua
@@ -5,12 +5,14 @@ MyOptionHelper = {
       { '不睡觉', function (player)
           player:enableMove(true)
           player:thinks(0, '现在还不想休息。')
+          MyOptionHelper.removeClickBedLimit(player) -- 移除点击限制
         end
       },
       { '睡半个时辰', function (player)
           player:enableMove(true)
           MyStoryHelper.addHour(1)
           PlayerHelper.everyPlayerNotify('时间过去半个时辰')
+          MyOptionHelper.removeClickBedLimit(player) -- 移除点击限制
         end
       },
       { '睡一个时辰', function (player)
@@ -21,6 +23,7 @@ MyOptionHelper = {
           else
             PlayerHelper.notifyGameInfo2Self(player.objid, '快到约定的时间了，不能睡那么久了')
           end
+          MyOptionHelper.removeClickBedLimit(player) -- 移除点击限制
         end
       },
       { '睡两个时辰', function (player)
@@ -31,6 +34,7 @@ MyOptionHelper = {
           else
             PlayerHelper.notifyGameInfo2Self(player.objid, '快到约定的时间了，不能睡那么久了')
           end
+          MyOptionHelper.removeClickBedLimit(player) -- 移除点击限制
         end
       },
     },
@@ -136,6 +140,7 @@ function MyOptionHelper.exitChoose (player)
   player.whichChoose = nil
   player:enableMove(true, true)
   player:thinkSelf(0, '算了，我都知道了。')
+  TimeHelper.delFnCanRun(10, player.objid .. 'clickBookcase') -- 解除点击限制
 end
 
 -- 显示选项
@@ -169,4 +174,14 @@ function MyOptionHelper.setTaskOption (player, tasks)
     end
   })
   MyOptionHelper.setOption(player.objid .. '', chooseItems)
+end
+
+--[[
+  移除玩家点击床后短时间内不能再次点击床的限制
+  @param    {table} player 玩家
+  @author   莫小仙
+  @datetime 2021-10-02 22:33:29
+]]
+function MyOptionHelper.removeClickBedLimit (player)
+  TimeHelper.delFnCanRun(10, player.objid .. 'clickBed')
 end
